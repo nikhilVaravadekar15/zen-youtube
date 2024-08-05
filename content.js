3ea99b2f29205a93e2e4038a2b65e5a3afb49600
nikhilvaravadekar <160208151+nikhilvaravadekar@users.noreply.github.com>
2024-08-05 15:06:02 +0530

    content scripts added

---
+++ content.js
@@ -0,0 +1,7 @@
+// Function to apply our custom styles
+function applyFocusMode() {
+  document.body.classList.add('youtube-zen-mode');
+}
+
+// Apply focus mode when the page loads
+applyFocusMode();
